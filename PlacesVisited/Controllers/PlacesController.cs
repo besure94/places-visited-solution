a8304557ef48aa04079d677c264a4bc4bfd929a4
Brian Scherner <brianscherner@gmail.com>
2023-10-04 15:34:29 -0700

    Add functionality for getting countryName + passing test.

--- PlacesVisited/Controllers/PlacesController.cs
+++ PlacesVisited/Controllers/PlacesController.cs
@@ -20,9 +20,9 @@ namespace PlacesVisited.Controllers
     }
 
     [HttpPost("/places")]
-    public ActionResult Create(string cityName, string lengthOfStay, string companions, string journalEntry)
+    public ActionResult Create(string cityName, string countryName, string lengthOfStay, string companions, string journalEntry)
     {
-      Place newPlace = new Place(cityName, lengthOfStay, companions, journalEntry);
+      Place newPlace = new Place(cityName, countryName, lengthOfStay, companions, journalEntry);
       return RedirectToAction("Index");
     }
 
--- PlacesVisited/Models/Place.cs
+++ PlacesVisited/Models/Place.cs
@@ -5,12 +5,14 @@ namespace PlacesVisited.Models
   public class Place
   {
     public string CityName { get; set; }
+    public string CountryName { get; }
     public string LengthOfStay { get; set; }
     public string Companions { get; set; }
     public string JournalEntry { get; set; }
-    public Place(string cityName, string lengthOfStay, string companions, string journalEntry)
+    public Place(string cityName, string countryName, string lengthOfStay, string companions, string journalEntry)
     {
       CityName = cityName;
+      CountryName = countryName;
       LengthOfStay = lengthOfStay;
       Companions = companions;
       JournalEntry = journalEntry;
