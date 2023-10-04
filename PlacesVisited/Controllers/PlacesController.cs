107cbb7ab13efc38e9548c3b02e2ecbcfb7587ea
Brian Scherner <brianscherner@gmail.com>
2023-10-04 13:52:36 -0700

    Add functionality to add new places with form to add info.

--- PlacesVisited/Controllers/PlacesController.cs
+++ PlacesVisited/Controllers/PlacesController.cs
@@ -6,6 +6,38 @@ namespace PlacesVisited.Controllers
 {
   public class PlacesController: Controller
   {
+    // [HttpGet("/places")]
+    // public ActionResult Index()
+    // {
+    //   List<Place> allPlaces = Place.GetAll();
+    //   return View(allPlaces);
+    // }
 
+    [HttpGet("/places/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/places")]
+    public ActionResult Create(string cityName)
+    {
+      Place newPlace = new Place(cityName);
+      return RedirectToAction("Index");
+    }
+
+    // [HttpPost("/places/delete")]
+    // public ActionResult DeleteAll()
+    // {
+    //   Place.ClearAll();
+    //   return View();
+    // }
+
+    // [HttpGet("/places/{id}")]
+    // public ActionResult Show(int id)
+    // {
+    //   Place foundPlace = Place.Find(id);
+    //   return View(foundPlace);
+    // }
   }
 }
